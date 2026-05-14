1737262c25154460b1a0ede5c557ec145376dd00
Autumn Green <me@axolotl-logic.io>
2026-05-14 08:54:21 -0400

    first commit

---
+++ quartz.config.ts
@@ -0,0 +1,97 @@
+import { QuartzConfig } from "./quartz/cfg"
+import * as Plugin from "./quartz/plugins"
+
+/**
+ * Quartz 4 Configuration
+ *
+ * See https://quartz.jzhao.xyz/configuration for more information.
+ */
+const config: QuartzConfig = {
+  configuration: {
+    pageTitle: "Autumn's Garden",
+    pageTitleSuffix: "",
+    enableSPA: false,
+    enablePopovers: true,
+    analytics: {
+      provider: "plausible",
+    },
+    locale: "en-US",
+    baseUrl: "garden.axolotl-logic.io",
+    ignorePatterns: ["private", "templates", ".obsidian"],
+    defaultDateType: "modified",
+    theme: {
+      fontOrigin: "googleFonts",
+      cdnCaching: true,
+      typography: {
+        header: "Schibsted Grotesk",
+        body: "Source Sans Pro",
+        code: "IBM Plex Mono",
+      },
+      colors: {
+        lightMode: {
+          light: "#fdf8f0",
+          lightgray: "#f0e0c8",
+          gray: "#bb9457",
+          darkgray: "#432818",
+          dark: "#1a0a08",
+          secondary: "#6f1d1b",
+          tertiary: "#99582a",
+          highlight: "rgba(187, 148, 87, 0.15)",
+          textHighlight: "#ffe6a788",
+        },
+        darkMode: {
+          light: "#1c0d0b",
+          lightgray: "#3d1f1d",
+          gray: "#6f4030",
+          darkgray: "#e8d5b0",
+          dark: "#ffe6a7",
+          secondary: "#bb9457",
+          tertiary: "#99582a",
+          highlight: "rgba(187, 148, 87, 0.15)",
+          textHighlight: "#bb945788",
+        },
+      },
+    },
+  },
+  plugins: {
+    transformers: [
+      Plugin.FrontMatter(),
+      Plugin.CreatedModifiedDate({
+        priority: ["frontmatter", "git", "filesystem"],
+      }),
+      Plugin.SyntaxHighlighting({
+        theme: {
+          light: "github-light",
+          dark: "github-dark",
+        },
+        keepBackground: false,
+      }),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.GitHubFlavoredMarkdown(),
+      Plugin.TableOfContents(),
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.Description(),
+      Plugin.Latex({ renderEngine: "katex" }),
+    ],
+    filters: [Plugin.RemoveDrafts()],
+    emitters: [
+      Plugin.AliasRedirects(),
+      Plugin.ComponentResources(),
+      Plugin.ContentPage(),
+      Plugin.FolderPage(),
+      Plugin.TagPage(),
+      Plugin.ContentIndex({
+        enableSiteMap: true,
+        enableRSS: true,
+      }),
+      Plugin.Assets(),
+      Plugin.Static(),
+      Plugin.Favicon(),
+      Plugin.NotFoundPage(),
+      // Comment out CustomOgImages to speed up build time
+      Plugin.CustomOgImages(),
+    ],
+  },
+}
+
+export default config
